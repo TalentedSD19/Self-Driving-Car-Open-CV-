273261e0f5d42fc7aee15a3f7c8a10a60d0bc749
Soumyajit Datta <98849191+TalentedSD19@users.noreply.github.com>
2023-04-11 23:42:40 +0530

    Added LCD code

--- motorshield/motorshield.ino
+++ motorshield/motorshield.ino
@@ -1,3 +1,6 @@
+#include<Wire.h>
+#include<LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,16,2);
 #include <AFMotor.h>
 
 AF_DCMotor motor1(1);
@@ -6,7 +9,10 @@ AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
 
 void setup() {
-  Serial.begin(9600); // set up Serial library at 9600 bps
+  // put your setup code here, to run once:
+lcd.init();
+lcd.backlight();
+Serial.begin(9600);
 }
 void move(char m)
 {
@@ -21,20 +27,14 @@ void move(char m)
     motor4.setSpeed(255);
     motor4.run(FORWARD);
     Serial.println("FORWARD");
+    lcd.setCursor(0,0);
+    lcd.print("MOVE");
+    lcd.setCursor(0,1);
+    lcd.print("FORWARD");
+    delay(100);
+    lcd.clear();
   }
-  if(m=='B')
-  {
-    motor1.setSpeed(255);
-    motor1.run(BACKWARD);
-    motor2.setSpeed(255);
-    motor2.run(BACKWARD);
-    motor3.setSpeed(255);
-    motor3.run(BACKWARD);
-    motor4.setSpeed(255);
-    motor4.run(BACKWARD);
-    Serial.println("BACKWARD");
-  }
-  if(m=='R')
+  else if(m=='R')
   {
     motor1.setSpeed(255);
     motor1.run(BACKWARD);
@@ -45,8 +45,14 @@ void move(char m)
     motor4.setSpeed(255);
     motor4.run(FORWARD);
     Serial.println("RIGHT");
+    lcd.setCursor(0,0);
+    lcd.print("MOVE");
+    lcd.setCursor(0,1);
+    lcd.print("RIGHT");
+    delay(100);
+    lcd.clear();
   }
-  if(m=='L')
+  else if(m=='L')
   {
     motor1.setSpeed(255);
     motor1.run(FORWARD);
@@ -57,34 +63,55 @@ void move(char m)
     motor4.setSpeed(255);
     motor4.run(BACKWARD);
     Serial.println("LEFT");
+    lcd.setCursor(0,0);
+    lcd.print("MOVE");
+    lcd.setCursor(0,1);
+    lcd.print("LEFT");
+    delay(100);
+    lcd.clear();
   }
-  if(m=='S')
+  else if(m=='RS')
   {
   motor1.run(RELEASE);
   motor2.run(RELEASE);  
   motor3.run(RELEASE);
   motor4.run(RELEASE);
   Serial.println("STOP");
+lcd.setCursor(0,0);
+lcd.print("Red signal");
+lcd.setCursor(0,1);
+lcd.print("Stop");
+delay(100);
+lcd.clear();
+  }
+   else if(m=='LS')
+  {
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);  
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  Serial.println("STOP");
+  lcd.setCursor(0,0);
+  lcd.print("NO LANE");
+  lcd.setCursor(0,1);
+  lcd.print("Stop");
+  delay(100);
+  lcd.clear();
   }
 
 }
 
 void loop() {
-  char d='S';
-while(Serial.available())
-{
-  d=Serial.read();
-  if(d=='F')
+  // put your main code here, to run repeatedly:
+  // lcd.clear();
   move('F');
-  if(d=='B')
-  move('B');
-  if(d=='L')
+  delay(2500);
+  move('RS');
+  delay(2500);
   move('L');
-  if(d=='R')
+  delay(2500);
   move('R');
-  if(d=='S')
-  move('S');
-}
-
-  
+  delay(2500);
+  move('LS');
+  delay(2500);
 }
